2d39968ef46e602e04e6af7ae889f91cf2971125
= <=>
2025-02-24 17:59:46 +0430

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let numberOne = 8; //statement
+numberOne ="Hi and how are you!";
+
+const numberTwo = 10; //statement
+// weakly number String 
+//number
+// Tailwind DOM 
+//String 'ahmad' "Ahmad"
+
