44352f6342a03b5eafb22cf609508aee6b588e34
ashrafomid2025 <omidmashraf@gmail.com>
2025-02-26 16:07:26 +0430

    final-touch1

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-let numberOne = 8; //statement
-numberOne ="Hi and how are you!";
+let computer = {
+    'model': 7270,
+    'HardDiskSize':'256GB',
+    'RamSize': `8GB`,
+    'BatteryHealth': 'Excellent'
+}
+//dot notation bracket notation
+// Array 5, 6,7,[4,'', true , {},]
+console.log(computer.RamSize);
+let People = [
+    {'name': 'Ali', age: 32},
+    {'name': 'Layla', age: 78}
+];
+// 
 
-const numberTwo = 10; //statement
-// weakly number String 
-//number
-// Tailwind DOM 
-//String 'ahmad' "Ahmad"
+
+
 
